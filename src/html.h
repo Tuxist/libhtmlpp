efecf33c87e72d6e841c3fd2d4b1c4f0d408985b
Jan Koester <jan.koester@gmx.net>
2022-08-19 20:11:55 +0200

    fixed header

--- src/html.h
+++ src/html.h
@@ -25,7 +25,7 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *******************************************************************************/
 
-#include <type.h>
+#include <sys/types.h>
 
 #include <systempp/sysarray.h>
 
